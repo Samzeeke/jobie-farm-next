3fa3cd9621d723193fc6fc23939f03f10906bb94
Joel Ojerinde <joelojerinde@gmail.com>
2023-02-23 18:16:11 +0100

    feat: store users in the backend

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,47 +1,23 @@
-import React from 'react'
-import Footer from '../../components/Footer'
-import Header from '../../components/Header'
-import { useAuth } from '../../contexts/auth.context'
-
+import React from "react";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import { useAuth } from "../../contexts/auth.context";
+import classes from "./User.module.css";
 export default function Profile() {
-  const { authUser, loading } = useAuth()
+  const { authUser, loading } = useAuth();
   return (
     <>
       <Header />
-      <div className="flex md:flex-row sm:flex-col justify-center align-middle">
-        <div className="p-4">
-          <div>Display name: {authUser?.displayName ?? ''}</div>
-          <div>Email: {authUser?.email ?? ''}</div>
-        </div>
-        <div className="">
-          <div className="border-solid border-slate-50">
-            <div>
-              <input placeholder="First name" />
-            </div>
-            <div>
-              <input placeholder="Last name" />
-            </div>
-          </div>
-          <div className="border-solid border-slate-50">
-            <div>
-              <input placeholder="Email address" readOnly />
-            </div>
-          </div>
-          <div>
-            <input placeholder='address'/>
-          </div>
-          <div className="border-solid border-slate-50">
-            <div>
-              <input placeholder="Password" />
-            </div>
-
-            <div>
-              <input placeholder="Confirm Password" />
-            </div>
+      <div className={classes.container}>
+        <div className={classes.profile}>
+          <div className={classes.profile__details}>
+            <h1 className={classes.h1}>Null</h1>
+            <p className={classes.p}>Email: {authUser?.email}</p>
+            <p className={classes.p}>Phone Number: null</p>
           </div>
         </div>
       </div>
       <Footer />
     </>
-  )
+  );
 }
